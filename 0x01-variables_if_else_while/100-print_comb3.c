9364c23f80a0955227062c21be8fa26ca17e9c4c
NohaAdel32 <love.queen1921989@gmail.com>
2023-07-15 16:11:00 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,14 +8,14 @@ int main(void)
 {
 	int a = 0;
 
-	while (a <= '0')
+	while (a <= 99)
 	{
 		putchar((a / 10) + '0');
 		putchar((a % 10) + '0');
+		putchar(',');
+		putchar(' ');
 		a++;
 	}
-	putchar(',');
-	putchar(' ');
 	putchar('\n');
 	return (0);
 }
