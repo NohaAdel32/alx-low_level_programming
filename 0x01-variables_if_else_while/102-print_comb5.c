3e7d7af465b60cbcc4624437c495b8dfb3c680d8
NohaAdel32 <love.queen1921989@gmail.com>
2023-07-15 18:01:33 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,15 +6,24 @@
  */
 int main(void)
 {
-	int a = 0;
+	int i, j;
 
-	while (a <= 99)
+	for (i = 0; i <= 98; i++)
 	{
-		putchar((a / 10) + '0');
-		putchar((a % 10) + '0');
-		putchar(',');
-		putchar(' ');
-		a++;
+		for ((j = i + 1); j <= 99; j++)
+		{
+			putchar((i / 10) + 48);
+			putchar((i % 10) + 48);
+			putchar(' ');
+			putchar((j / 10) + 48);
+			putchar((j % 10) + 48);
+
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
 	putchar('\n');
 	return (0);
