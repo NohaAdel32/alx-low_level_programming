e4638fc84d618b9318c036d2d22a000d78e3ee60
NohaAdel32 <love.queen1921989@gmail.com>
2023-07-19 14:55:34 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include"main.h"
+
+/**
+ * print_times_table - prints the n times table
+ * @n: times table
+ * Return: void
+ */
+
+
+void print_times_table(int n)
+{
+	int prod, n1, n2;
+
+	if (n <= 15 && n >=0)
+	{
+		for (n1 = 0, n1 <= n, n1++)
+		{
+			_putchar(48)
+
+				for (n2 = 1, n2 <= n, n2++)
+				{
+					_putchar(',');
+					_putchar(' ');
+					prod = n1 * n2;
+
+					if (prod <= 9)
+						_putchar(prod + '0');
+					else
+					{
+						_putchar((prod / 10) + '0');
+						_Putchar((prod % 10) + '0');
+					}
+				}
+			_putchar('\n');
+		}
+	}
+}
+
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,35 +7,25 @@
 
 void times_table(void)
 {
-	int n, i, t;
+	int n, i, prod;
 
 	for (n = 0; n <= 9; n++)
 	{
+		_putchar(48);
+
 		for (i = 0; i <= 9; i++)
 		{
-			t = i * n;
+			_putchar(',');
+			_putchar(' ');
+			prod = i * n;
 
-			if (i != 9)
-			{
-				if (t <= 9)
-					_putchar(t + '0');
-				else
-				{
-				_putchar((t / 10) + '0');
-				_putchar((t % 10) + '0');
-				}
-				_putchar(',');
-				_putchar(' ');
-			}
+			if (prod <= 9)
+				_putchar(prod + '0');
+
 			else
 			{
-				if (t <= 9)
-					_putchar(t + '0');
-				else
-				{
-				_putchar((t / 10) + '0');
-				_putchar((t % 10) + '0');
-				}
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
 			}
 		}
 		_putchar('\n');
