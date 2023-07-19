0aac10f8e3343c66af7493c199f8e8396a4efa94
NohaAdel32 <love.queen1921989@gmail.com>
2023-07-19 15:45:11 -0700

    void print_time

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,7 +11,7 @@ void print_times_table(int n)
 {
 	int prod, n1, n2;
 
-	if (n <= 15 && n >=0)
+	if (n <= 15 && n >= 0)
 	{
 		for (n1 = 0, n1 <= n, n1++)
 		{
@@ -24,11 +24,23 @@ void print_times_table(int n)
 					prod = n1 * n2;
 
 					if (prod <= 9)
-						_putchar(prod + '0');
-					else
 					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(prod + '0');
+					}
+					else if (prod <= 99)
+					{
+						_putchar(' ');
 						_putchar((prod / 10) + '0');
 						_Putchar((prod % 10) + '0');
+					}
+					else
+					{
+						_putchar((prod / 100) + '0');
+						prod = prod % 100;
+						_putchar((prod / 10) + '0');
+						_putchar((prod % 10) + '0');
 					}
 				}
 			_putchar('\n');
