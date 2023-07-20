6ca5d410dad4403f68c86fe530f9c79d055f7c94
NohaAdel32 <love.queen1921989@gmail.com>
2023-07-19 18:45:37 -0700

    M

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long sum;
+	unsigned long fib1 = 0;
+	unsigned long fib2 = 1;
+
+	int count;
+
+	for (count = 0; count <= 98; count++)
+	{
+		sum = fib1 + fib2;
+
+		if (count == 98)
+			printf("%lu\n", sum);
+		else
+		{
+			fib1 = fib2;
+			fib2 = sum;
+
+			printf("%lu,", sum);
+		}
+	}
+	return (0);
+}
