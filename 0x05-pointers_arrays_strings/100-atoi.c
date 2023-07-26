7401ec45670d17c9b7d026a6c2dcb0c0f25c2530
NohaAdel32 <love.queen1921989@gmail.com>
2023-07-25 18:19:08 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,7 +15,7 @@ int _atoi(char *s)
 	{
 		if (*s == '-')
 			sign *= -1;
-		else if ((*s > '0') && (*s < '9'))
+		else if ((*s >= '0') && (*s <= '9'))
 			break;
 		else if ((*s < '0') && (*s > '9'))
 			break;
