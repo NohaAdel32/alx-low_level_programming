123ccbae222132d1a005fa545a423e44f9ba7ff8
NohaAdel32 <love.queen1921989@gmail.com>
2023-07-26 19:09:27 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * string_toupper - change all lowercase to uppercase
+ * @s: string to transform
+ * Return: pointer to transformed string
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if ((s[i] > 97) && (s[i] < 122))
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
