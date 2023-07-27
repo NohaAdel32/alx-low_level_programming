a1c1dc14490b2984b7845bd062b9dd8cc7e80bf7
NohaAdel32 <love.queen1921989@gmail.com>
2023-07-26 17:03:15 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: a string
+ * @src: a string
+ * Return: dest
+ */
+
+
+char *_strcat(char *dest, char *src)
+{
+	int c, c2;
+
+	while (dest[c] != '\0')
+		c++;
+
+	for (c2 = 0; src[c2] != '\0'; c2++)
+	{
+		dest[c] = src[c2];
+		c++;
+	}
+	return (dest);
+}
