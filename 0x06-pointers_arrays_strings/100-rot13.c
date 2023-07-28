1c781a86845a8125aea7e1ba90f735f3c9217234
NohaAdel32 <love.queen1921989@gmail.com>
2023-07-27 17:33:28 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * rot13 - Encode a string to rot13
+ * @s: string to encode
+ * Return: pointer to encoded string
+ */
+
+char *rot13(char *s)
+{
+	char *r = s;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; a[i] != '\0'; i++)
+		{
+			if (*s == a[i])
+			{
+				*s = b[i];
+			}
+		}
+		s++;
+	}
+	return (r);
+}
