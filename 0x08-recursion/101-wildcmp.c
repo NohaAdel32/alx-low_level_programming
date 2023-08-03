a8cf02cba695e0883556ea5c7efa8e1a2411032c
NohaAdel32 <love.queen1921989@gmail.com>
2023-08-02 17:21:32 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,19 +8,17 @@
  * @j: special index. (joker)
  * Return: 1 if s is palindrome, 0 otherwise.
  */
-
 int str_checker(char *s1, char *s2, int i, int j)
 {
 	if (s1[i] == '\0' && s2[j] == '\0')
 		return (1);
-	else if (s1[i] == s2[j])
+	if (s1[i] == s2[j])
 		return (str_checker(s1, s2, i + 1, j + 1));
-	else if (s2[j] == '*')
+	if (s1[i] == '\0' && s2[j] == '*')
 		return (str_checker(s1, s2, i, j + 1));
-	else if (s1[i] == '\0' && s2[j] == '*')
-		return (str_checker(s1, s2, i, j + 1));
-	else
-		return (0);
+	if (s2[j] == '*')
+		return (str_checker(s1, s2, i + 1, j) || str_checker(s1, s2, i, j + 1));
+	return (0);
 }
 /**
  * wildcmp - check if strings could be considered identical
