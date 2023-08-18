7c58b9527dad343e216bdec7bcb23af8081d068e
NohaAdel32 <love.queen1921989@gmail.com>
2023-08-17 17:06:03 -0700

    n

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,8 +17,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(sum_ptr, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(sum_ptr; int);
-
+		sum += va_arg(sum_ptr, int);
 	va_end(sum_ptr);
 	return (sum);
 }
