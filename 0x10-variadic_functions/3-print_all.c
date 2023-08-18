4b2b41e34b65921ddd554b92c254971324adba32
NohaAdel32 <love.queen1921989@gmail.com>
2023-08-17 19:57:27 -0700

    kV

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+
 /**
  * print_all - function with 2 parameter
  * @format: char type pointer to string
@@ -8,14 +9,13 @@
  */
 void print_all(const char * const format, ...)
 {
-	unsigned int i;
+	int j;
 	char *str;
 	char *space;
 	va_list ap;
 
 	va_start(ap, format);
 	j = 0;
-
 	while (format && format[j])
 	{
 		space = "";
@@ -23,24 +23,24 @@ void print_all(const char * const format, ...)
 			space = ", ";
 		switch (format[j])
 		{
-			case 'c':
-				printf("%c%s", va_arg(ap, int), space);
-				break;
-			case 'i':
-				printf("%i%s", va_arg(ap, int), space);
-				break;
-			case 'f':
-				printf("%f%s", va_arg(ap, float), space);
-				break;
-			case 's':
-				str = va_arg(ap, char *);
-				if (!str || !*str)
-					str = "(nil)";
-				printf("%s%s", str, space);
-				break;
+
+		case 'c':
+			printf("%c%s", va_arg(ap, int), space);
+			break;
+		case 'i':
+			printf("%d%s", va_arg(ap, int), space);
+			break;
+		case 'f':
+			printf("%f%s", va_arg(ap, double), space);
+			break;
+		case 's':
+			str = va_arg(ap, char *);
+			if (!str || !*str)
+				str = "(nil)";
+			printf("%s%s", str, space);
+			break;
 		}
 		j++;
 	}
 	printf("\n");
-	va_arg(ap);
 }
