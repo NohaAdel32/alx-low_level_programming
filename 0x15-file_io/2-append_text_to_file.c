b89b1cf886f6f4cb4f2b8c7d2311064766d841f8
NohaAdel32 <love.queen1921989@gmail.com>
2023-09-04 18:12:50 -0700

    vcg

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,24 +14,26 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, i = 0;
+	int i = 0, fl;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
+
 	if (text_content == NULL)
 		text_content = "";
+
 
 	while (text_content[i] != '\0')
 	{
 		i++;
 	}
-	
-	fd = open(filename, O_WRONLY | O_APPEND);
 
-	if (fd == -1)
+	fl = open(filename, O_WRONLY | O_APPEND);
+
+	if (fl == -1)
 		return (-1);
 
-	write(fd, text_content, i);
+	write(fl, text_content, i);
 
 	return (1);
 }
