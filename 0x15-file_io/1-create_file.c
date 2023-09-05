fe2471c84c618b0d8ce81dd32db8333dd4ba9812
NohaAdel32 <love.queen1921989@gmail.com>
2023-09-04 17:30:14 -0700

    hg

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+/**
+ * create_file - creates a file and puts text in it
+ * with 600 perms (do not change if it exists)
+ *
+ * @filename: name for file
+ * @text_content: text to put into file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i = 0;
+	int length;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	
+	if (fd == -1)
+		return (-1);
+
+	while (text_content[i] != '\0')
+	{
+		i++;
+		length = write(fd, text_content, i);
+	}
+	
+	if (close(fd) == -1 || length != i)
+		return (-1);
+	return (1);
+}
+
